ba309d64501314f8d5570c9cc2ad08e24e240375
Ubuntu <ubuntu@ip-172-31-14-51.ap-northeast-1.compute.internal>
2021-07-15 09:28:15 +0000

    working on the main scripts

--- config/cfg.py
+++ config/cfg.py
@@ -5,3 +5,6 @@ BASEPATH = "/work"
 
 DATA_DIR = os.path.join(BASEPATH, "data")
 MODELS_DIR = os.path.join(BASEPATH, "models")
+
+JP_SENTENCES_FILE = os.path.join(BASEPATH, "jp_sentences.txt")
+EN_SENTENCES_FILE = os.path.join(BASEPATH, "en_sentences.txt")
